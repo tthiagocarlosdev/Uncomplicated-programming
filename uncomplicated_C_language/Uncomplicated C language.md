d8389f05a40a836dbae0f7f1e8b23568e2c36868
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-09-17 10:17:56 -0300

    aula_01_-_esqueleto_de_um_programa_em_c

---
+++ uncomplicated_C_language/Uncomplicated C language.md
@@ -0,0 +1,4 @@
+# Uncomplicated C language
+
+Play list uncomplicated c language - [classes link](https://www.youtube.com/watch?v=GiCt0Cwcp-U&list=PL8iN9FQ7_jt4DJbeQqv--jpTy-2gTA3Cp)
+
---
+++ uncomplicated_C_language/aula_01_-_esqueleto_de_um_programa_em_c.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // escrita e leitura
+#include <stdlib.h> // biblioteca auxiliar
+
+int main(){
+  
+  printf("Hello World!\n");
+  system("pause");
+
+  return 0;
+}
